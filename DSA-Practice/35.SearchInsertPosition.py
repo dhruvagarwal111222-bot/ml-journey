30bbe35ea5d1f2e2c7f5916d2dde12bc1c3c47fc
Dhruv Agarwal <dhruvagarwal111222@gmail.com>
2026-08-29 10:00:04 +0530

    day 13 fixes

--- DSA-Practice/35.SearchInsertPosition.py
+++ DSA-Practice/35.SearchInsertPosition.py
@@ -1,5 +1,5 @@
 class Solution:
-    def searchInsert(self, nums: List[int], target: int) -> int:
+    def searchInsert(self, nums: list[int], target: int) -> int:
         left, right = 0, len(nums)-1
         while left <= right:
             mid = (left + right) // 2
